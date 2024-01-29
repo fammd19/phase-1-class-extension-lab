5b5e0ee3668a0de60c526a990b3a9f97f6e8fe82
Fi Mellink <fimellink@gmail.com>
2024-01-29 12:48:37 +1100

    Complete lab@

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+class Polygon {
+    constructor(sides) {
+        this.sides = sides;
+    }
+    get countSides () {
+        return this.sides.length;
+    }
+    get perimeter () {
+        return this.sides.reduce((acc, side) => acc+side,0);
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid () {
+    if (this.countSides ==! 3) return false;
+
+    const [a, b, c] = this.sides;
+    return a + b > c && a + c > b && b + c > a;
+    }
+}
+
+
+class Square extends Polygon {
+    get isValid () {
+        if (this.sides[0] === this.sides[1] && 
+            this.sides[1] === this.sides[2] &&
+            this.sides[2] === this.sides[3])
+            {
+                return true;
+            }
+        else {
+            return false;
+        } 
+    }
+    get area () {
+        return this.sides[0]**2;
+    }
+}
